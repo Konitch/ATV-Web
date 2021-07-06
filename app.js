de26dd385dd2076f521912049b567d759e4cbc17
Konitch <56593997+Konitch@users.noreply.github.com>
2021-07-06 15:30:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -37,6 +37,6 @@ app.get('/numeroaleatorio', function(req,res){
 
 
 //--------Server----------------
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Servidor rodando com Express");
 });
